21eeefd453e89a5f5ec3b17817784a1fbdb52685
deepak <deepakmarudih786@gmail.com>
2024-06-13 15:27:28 +0530

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BackendController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+Route::get('/backend/dashboard',[BackendController::class, 'index'])->name('index');
